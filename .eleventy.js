ac4dd51fecb6dfd2066a4a5fead6b953ed5b76f9
Nikola Đuza <nikolaseap@gmail.com>
2019-11-17 18:46:51 +0100

    Hide header when scrolling

--- .eleventy.js
+++ .eleventy.js
@@ -36,6 +36,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addCollection("tagList", require("./_11ty/getTagList"));
 
   eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy("css");
 
   /* Markdown Overrides */
