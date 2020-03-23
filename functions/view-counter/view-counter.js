edfcfc36957be93c88934e76f345f2c66860ca52
Nikola Đuza <nikolaseap@gmail.com>
2020-03-23 19:06:30 +0100

    Lose callback function

--- functions/view-counter/view-counter.js
+++ functions/view-counter/view-counter.js
@@ -1,23 +1,25 @@
 const { parse } = require("url")
 const increment = require("./increment-views")
 
-exports.handler = async (event, context, callback) => {
+exports.handler = async (event, context) => {
   console.log(event.body)
   const { slug } = JSON.parse(event.body)
 
-  if (!slug) callback(null, {
-    statusCode: 500,
-    body: "Error occured: Missing `slug` parameter"
-  })
+  if (!slug) {
+    return {
+      statusCode: 500,
+      body: "Error occured: Missing `slug` parameter"
+    }
+  }
 
   console.log(`Started incrementing view counter for ${slug}`)
   const { snapshot } = await increment(slug)
   console.log(`Finished incrementing view counter for ${slug}`)
 
-  return callback(null, {
+  return {
     statusCode: 200,
     body: JSON.stringify({
       total: snapshot.val()
     })
-  })
+  }
 }
