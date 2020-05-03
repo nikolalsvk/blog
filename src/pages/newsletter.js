e8bee31c718c2510f6e6c4b3ab36a9498ac27f23
Nikola Đuza <nikolaseap@gmail.com>
2020-05-03 23:18:38 +0200

    Add newsletter page, hide post view count for now

---
+++ src/pages/newsletter.js
@@ -0,0 +1,45 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import SubscribeForm from "../components/subscribe-form"
+
+const NewsletterPage = ({
+  data: {
+    site: {
+      siteMetadata: { title },
+    },
+  },
+  location,
+}) => (
+  <Layout location={location} title={title}>
+    <div>
+      <h1>Get Blog Updates</h1>
+
+      <p>
+        Why should you subscribe? Well, why not? If you like some of my blog
+        posts or talks, I will send you an email when I release a new one. That
+        means <b>FREE</b> knowledge, tips, and pineapples 🍍.
+      </p>
+
+      <p>
+        At most, it will be a couple of emails a year. Can your mailbox undergo
+        that many emails? If the answer is yes, subscribe and let us keep in
+        touch. You can always unsubscribe later!
+      </p>
+
+      <SubscribeForm />
+    </div>
+  </Layout>
+)
+
+export default NewsletterPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
