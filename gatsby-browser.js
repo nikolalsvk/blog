86d6cc215ec08aa97cd3d581bc207c4dd3c7a819
Nikola Đuza <nikolaseap@gmail.com>
2020-11-20 21:01:09 +0100

    Revert "Remove flicker when navigating"

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,11 +7,7 @@ import "prismjs/themes/prism.css"
 import React from "react"
 
 import { ThemeProvider } from "./src/contexts/theme"
-import GlobalStyles from "./src/components/globalStyles"
 
 export const wrapRootElement = ({ element }) => (
-  <ThemeProvider>
-    <GlobalStyles />
-    {element}
-  </ThemeProvider>
+  <ThemeProvider>{element}</ThemeProvider>
 )
