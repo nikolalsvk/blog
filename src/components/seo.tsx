20926fc407cf0ba2d10c01edfe7d1f7de3c22e02
Nikola Đuza <nikolaseap@gmail.com>
2021-10-16 12:26:34 +0200

    Update default OG image

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
-import defaultOgImage from "../../content/assets/pineapple-emoji.png"
+import defaultOgImage from "../../content/assets/default-og-image.jpg"
 
 interface Props {
   title: string
