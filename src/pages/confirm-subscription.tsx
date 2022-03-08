d18a57576a31164361f68119031eff934b7ae690
Nikola Đuza <nikolaseap@gmail.com>
2022-03-08 16:48:04 +0100

    Change /confirm-subscription to show proper copy

--- src/pages/confirm-subscription.tsx
+++ src/pages/confirm-subscription.tsx
@@ -19,24 +19,35 @@ const ConfirmSubscriptionPage = ({ data }: Props) => {
 
   return (
     <Layout title={siteTitle} showLargeHeader>
-      <SEO title="Thank you for subscribing" />
+      <SEO title="Check inbox" />
 
       <article>
         <h2>
-          Subscription confirmed!{" "}
-          <span role="img" aria-label="tada">
-            🎉
+          Thank you for subscribing{" "}
+          <span role="img" aria-label="bow">
+            🙇
           </span>
         </h2>
-        <h3>
-          You are oficially on the list! I will send you any new content as soon
-          as it gets released.
-        </h3>
-        For now, you can{" "}
-        <Link style={{ boxShadow: `none` }} to="/">
-          head back to the blog
-        </Link>
-        .
+        <h2>Please check your inbox to confirm the subscription.</h2>
+        <p>
+          Once you confirm that you're willingly subscribing to the Pragmatic
+          Pineapple newsletter, the next issue will arrive in your inbox. If you
+          don't confirm, you will miss out on great content I share, so be sure
+          to do it.
+        </p>
+        <p>
+          I want to thank you for choosing to be a part of my newsletter,
+          amongst all other newsletters out there. It really means a lot to me
+          and it motivates me to produce and share good content with you. I know
+          our time in a day is limited and I voe to not spam you and only give
+          you the high-quality writing you deserve.
+        </p>
+        <p>
+          If you like my writing, be sure to check out the{" "}
+          <Link to="/about">about page</Link> to learn more about me, follow me
+          on <Link to="https://twitter.com/nikolalsvk">Twitter</Link>, or go
+          back to the <Link to="/home">home page</Link>.
+        </p>
       </article>
     </Layout>
   )
